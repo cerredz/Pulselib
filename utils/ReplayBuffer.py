d02d9051f35312749aa06180a222d475c462272d
unknown <422michaelcerreto@gmail.comgit config --global user.name>
2025-12-19 16:39:48 -0500

    replay buffer torch implementation

--- utils/ReplayBuffer.py
+++ utils/ReplayBuffer.py
@@ -1,4 +1,5 @@
 import random
+from turtle import position
 import torch
 
 class ReplayBuffer():
@@ -22,23 +23,52 @@ class ReplayBuffer():
         return len(self.buffer)
 
 
-class ReplayBufferTorch():
-    def __init__(self, device, batch_size, capacity):
-        self.device, self.batch_size, self.capacity = device, batch_size, capacity
-        self.ptr, self.size, self.storage = 0, 0, None
+# pytorch replay buffer class
+## (right now buffer lives in RAM, going to look into how to move to disk to store even bigger replays)
 
-    def add(self, s, a, r, ns, d):
+class ReplayBufferTorch:
+    def __init__(self, file_path: str, capacity: int, device: str, state_dim: int, action_dim: int):
+        assert state_dim > 0 and action_dim > 0
 
-        if self.storage is None:
-            self.storage = [torch.empty((self.capacity, *x.shape[1:]), dtype=x.dtype, device=self.device) for x in [s, a, r, ns, d]]
+        self.capacity=capacity
+        self.device=device
+        self.state_dim=state_dim
+        self.action_dim=action_dim
+
+        self.state_buffer=torch.zeros((capacity, self.state_dim), device=self.device, dtype=torch.int32)
+        self.action_buffer=torch.zeros((capacity, self.action_dim), device=self.device, dtype=torch.int32)
+        self.next_state_buffer=torch.zeros((capacity, self.state_dim), device=self.device, dtype=torch.int32)
+        self.reward_buffer=torch.zeros(capacity, device=self.device, dtype=torch.int32)
+        self.terminated_buffer=torch.zeros(capacity, device=self.device, dtype=torch.int32)
+
+        self.position=0
+        self.size=0
+        self.full_buffer=False # bool flag to determine if we had to reset the position back to 0 after filling up the buffer
+
+    def __len__(self):
+        return self.size
+
+    def add(self, state, action, reward, next_state, done):
+        batch_size=state.shape[0]
+
+        idx = torch.arange(self.position, self.position + batch_size, device=self.device) % self.capacity        
         
-        # Smart circular buffer using modulo (handles wrapping automatically)
-        idx = torch.arange(self.ptr, self.ptr + self.batch_size, device=self.device) % self.capacity
-        for buf, val in zip(self.storage, [s, a, r, ns, d]): buf[idx] = val
-        
-        self.ptr = (self.ptr + self.batch_size) % self.capacity
-        self.size = min(self.size + self.batch_size, self.capacity)
+        self.state_buffer[idx]=state
+        self.action_buffer[idx]=action
+        self.reward_buffer[idx] = reward
+        self.next_state_buffer[idx]=next_state
+        self.terminated_buffer[idx]=done
 
-    def sample(self):
-        idx = torch.randint(0, self.size, (self.batch_size,), device=self.device)
-        return [buf[idx] for buf in self.storage]
+        self.position = (self.position + batch_size) % self.capacity
+        self.size = min(self.size + batch_size, self.capacity)
+
+    def sample(self, batch_size: int):
+        idx=torch.randint(0, self.size, (batch_size,), device=self.device)
+
+        return (
+            self.state_buffer[idx],
+            self.action_buffer[idx],
+            self.reward_buffer[idx],
+            self.next_state_buffer[idx],
+            self.terminated_buffer[idx]
+        )
