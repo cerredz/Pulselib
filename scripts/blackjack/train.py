628dfecdbc41bbcf4647b8efd0e04520bd159714
unknown <422michaelcerreto@gmail.comgit config --global user.name>
2025-12-21 18:00:45 -0500

    actor critic neural network implementation

--- scripts/blackjack/train.py
+++ scripts/blackjack/train.py
@@ -1,7 +1,9 @@
+from agents.TemperalDifference.ActorCritic import ActorCritic
 from agents.TemperalDifference.DQN import DQN
 from agents.TemperalDifference.DoubleDQN import DoubleDQN
 from environments.blackjack.utils import blackjack_training_utils
 import torch
+from utils.agents import default_actor_critic_params
 from utils.config import get_config_file, get_result_folder
 from utils.plotting import plot_learning_curve
 from utils.torch import load_device
@@ -25,15 +27,17 @@ def train_agent(env, device, config, agent, results_dir):
 
         while terminated.float().mean() < .95:
             active_mask = ~terminated
+            states=states.float().to(device)
             actions = agent.action(states)
             next_states, rewards, dones, truncated, info = env.step(actions)
             episode_reward += rewards[active_mask].sum().item()
             total_steps += active_mask.sum().item()
+            next_states=next_states.float().to(device)
             agent.train_step(states[active_mask], actions[active_mask], rewards[active_mask], next_states[active_mask], dones[active_mask])
             terminated |= dones
             states = next_states
 
-        agent.decay_epsilon()
+        #agent.decay_epsilon()
         episode_rewards[i] = episode_reward
        
         if (i + 1) % 500 == 0:
@@ -45,7 +49,6 @@ def train_agent(env, device, config, agent, results_dir):
             steps_per_sec = total_steps / elapsed if elapsed > 0 else 0
             print(f"Episode {i+1:5d}/{n} | "
                   f"Avg Reward (last 50): {avg_reward:8.2f} | "
-                  f"Epsilon: {agent.epsilon:.4f} | "
                   f"Speed: {steps_per_sec:6.1f} steps/sec")
 
     scores = episode_rewards.detach().cpu().tolist()
@@ -69,7 +72,7 @@ if __name__ == "__main__":
         device=device,
         batch_size=config["BATCH_SIZE"]
     )
-
+    """
     agent = DoubleDQN(
         env_action_space=env.action_space,
         state_dim=config["STATE_DIM"],
@@ -87,5 +90,9 @@ if __name__ == "__main__":
         target_network=None,
         weights_path=None,
     )
+    """
+
+    params=default_actor_critic_params(env, config["STATE_DIM"], device)
+    agent=ActorCritic(**params)
 
     train_agent(env, device, config, agent, results_dir)
--- utils/agents.py
+++ utils/agents.py
@@ -1,6 +1,7 @@
-from agents.TemperalDifference.QLearning import QLearning
 import gymnasium as gym
-
+import torch
+import torch.nn as nn
+"""
 def get_agent(agent_id: str, env: gym.Env, config: dict):
     if agent_id == "q_learning":
         return QLearning(env, config)
@@ -8,3 +9,28 @@ def get_agent(agent_id: str, env: gym.Env, config: dict):
     #     return OnPolicyFirstVisitMC(...)
     else:
         raise ValueError(f"Unknown Agent ID: {agent_id}")
+"""
+
+def default_actor_critic_params(env, state_dim, device=torch.device("cpu")):
+    n_actions = env.action_space.n
+    
+    actor_network=nn.Sequential(
+        nn.Linear(state_dim, 32), nn.ReLU(), 
+        nn.Linear(32, n_actions), nn.Softmax(dim=-1)
+    )
+
+    critic_network=nn.Sequential(
+        nn.Linear(state_dim, 32), nn.ReLU(), 
+        nn.Linear(32, 1)
+    )
+
+    return {
+        'env_action_space': env.action_space,
+        'state_dim': state_dim,
+        'device': device,
+        'actor_network': actor_network,
+        'critic_network': critic_network,
+        'gamma': 0.99,
+        'learning_rate': 1e-3,
+        'weight_decay': 1e-4
+    }
